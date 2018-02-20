e59e85cf0d30c2530160f2235ff0641a9ad046af
mf3435 <mf3435@nyu.edu>
2018-02-20 00:21:22 -0500

    Fix popup

--- js/scripts.js
+++ js/scripts.js
@@ -14,74 +14,10 @@ var map = L.map('my-map').setView(defaultCenter, defaultZoom);
 
 var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-	subdomains: 'abcd'
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 17,
 }).addTo(map);
-
-
-  // Use L.geoJSON to load PLUTO parcel data that we clipped in QGIS and change the CRS from 2263 to 4326
-  // this was moved inside the getJSON callback so that the parcels will load on top of the study area study_boundary
-
-//Add Vacant Land
-  var landGeojson = L.geoJSON(land,
-    {
-      style: function(feature) {
-          return {
-            color: '#595959',
-            fillColor: '#e80000',
-            fillOpacity: 0.9,
-            weight: 1,
-          }
-//Popup not working
-					/*
-					onEachFeature: function(feature, layer) {
-//+'Owner:'+ +'Lot Area (sqft):'+
-                layer.bindPopup(`${feature.properties.Address}<br/>  ${feature.properties.OwnerName}<br/>
-                                ${feature.properties.LotArea}`, {
-                  closeButton: false,
-                  minWidth: 60,
-                  offset: [0, -10]
-                });
-                layer.on('mouseover', function (e) {
-                  this.openPopup();
-
-                  e.target.setStyle({
-                    weight: 3,
-                    color: '#FFF',
-                  });
-
-                  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-                      layer.bringToFront();
-                  }
-                });
-                layer.on('mouseout', function (e) {
-                  this.closePopup();
-                  blocksGeojson.resetStyle(e.target);
-                });
-              }*/
-
-//This popup doens't work either
-//.bindPopup(feature.properties.Address+ ' Owner: ' +  feature.properties.OwnerName+ 'Lot Area (sqft):'+ feature.properties.LotArea, {offset: [0, -6]})
-			},
-    }).addTo(map);
-
-			//Popup samples
-			//.bindPopup(placeObject.description1 + placeObject.placeName +  placeObject.description2);
-			// .bindPopup(pizzaObject.name + ' likes to eat at ' +  pizzaObject.pizzaShop, {offset: [0, -6]})
-
-//Add Gardens
-  var gardensGeojson = L.geoJSON(gardens,
-    {
-      style: function(feature) {
-          return {
-            color: '#595959',
-            fillColor: '#157a03',
-            fillOpacity: 0.8,
-            weight: 1,
-          }
-
-      },
-    }
-  ).addTo(map);
 
 //Add Community district boundaries
   var CDsGeojson = L.geoJSON(CDs,
@@ -97,6 +33,89 @@ var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fast
       },
     }
   ).addTo(map);
+
+  // Use L.geoJSON to load PLUTO parcel data that we clipped in QGIS and change the CRS from 2263 to 4326
+  // this was moved inside the getJSON callback so that the parcels will load on top of the study area study_boundary
+//Add vacant land
+	var landGeojson = L.geoJSON(land, {
+	  style: function(feature) {
+	      return {
+	        color: '#595959',
+	        fillColor: '#e80000',
+	        fillOpacity: 0.9,
+	        weight: 1,
+	      }
+		},
+		onEachFeature: function(feature, layer) {
+	    layer.bindPopup(`<h3>Vacant Lot</h3> at ${feature.properties.Address}.<br/><h3>//</h3>Owner: ${feature.properties.OwnerName}.<br/>
+	                    Area:    ${feature.properties.LotArea} sqft.<br/> FAR:    ${feature.properties.ResidFAR} residential; ${feature.properties.CommFAR} commercial; ${feature.properties.FacilFAR}; facilities`, {
+	      closeButton: false,
+	      minWidth: 60,
+	      offset: [0, -10]
+	    });
+	    layer.on('mouseover', function (e) {
+	      this.openPopup();
+
+	      e.target.setStyle({
+	        weight: 0.5,
+	        color: 'grey',
+					fillOpacity: 0.7,
+	      });
+
+	      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+	          layer.bringToFront();
+	      }
+	    });
+	    layer.on('mouseout', function (e) {
+	      this.closePopup();
+	      blocksGeojson.resetStyle(e.target);
+	    });
+		}
+	}).addTo(map);
+
+			//Popup samples
+			//.bindPopup(placeObject.description1 + placeObject.placeName +  placeObject.description2);
+			// .bindPopup(pizzaObject.name + ' likes to eat at ' +  pizzaObject.pizzaShop, {offset: [0, -6]})
+
+//Add Gardens
+//Garden_dev
+  var gardensGeojson = L.geoJSON(gardens,
+    {
+      style: function(feature) {
+          return {
+            color: '#595959',
+            fillColor: '#157a03',
+            fillOpacity: 0.8,
+            weight: 1,
+          }
+      },
+			onEachFeature: function(feature, layer) {
+				layer.bindPopup(`<h3>${feature.properties.Name}</h3>${feature.properties.Garden_dev} Community Garden at ${feature.properties.Address}.<br/><h3>//</h3>Owner: ${feature.properties.OwnerName}.<br/>
+												Area:    ${feature.properties.LotArea} sqft.<br/> FAR:    ${feature.properties.ResidFAR} residential; ${feature.properties.CommFAR} commercial; ${feature.properties.FacilFAR}; facilities`, {
+					closeButton: false,
+					minWidth: 60,
+					offset: [0, -10]
+				});
+				layer.on('mouseover', function (e) {
+					this.openPopup();
+
+					e.target.setStyle({
+						weight: 0.5,
+						color: 'grey',
+						fillOpacity: 0.7,
+					});
+
+					if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+							layer.bringToFront();
+					}
+				});
+				layer.on('mouseout', function (e) {
+					this.closePopup();
+					blocksGeojson.resetStyle(e.target);
+				});
+    }
+  }).addTo(map);
+
 
 	$('.zoomOut').click(function() {
   map.flyTo(defaultCenter, defaultZoom)
